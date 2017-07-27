6f5178c5e407ad4e7e912600d310e1dfc2d101e5
Thomas Le <tvle83@gmail.com>
2017-07-27 01:24:48 -0500

    Initial commit. Basic set up of redux app

---
+++ server.js
@@ -0,0 +1,14 @@
+"use strict";
+let express = require('express');
+let app = express();
+let path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.sendfile(path.resolve(__dirname, 'public', 'index.html'))
+});
+
+app.listen(3000, function(){
+    console.log('listening on port 3000...')
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    watch: true,
+    module:{
+        loaders: [
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                loader: 'bable-loader',
+                query: {
+                    presets: [ 'react', 'es2015', 'stage-1']
+                }
+            }
+        ]
+    }
+};
