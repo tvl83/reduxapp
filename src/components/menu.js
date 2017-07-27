c036d6f7b80a5967db14c6590a7b1ddf6bea42c2
Thomas Le <tvle83@gmail.com>
2017-07-27 17:20:38 -0500

    finished Section 2 Lecture 30
    display dynamic badge for item count on header

--- src/components/menu.js
+++ src/components/menu.js
@@ -14,18 +14,18 @@ class Menu extends React.Component{
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
-                        <NavItem eventKey={1}
-                                 href="/about">About</NavItem>
-                        <NavItem eventKey={2}
-                                 href="/contacts">Contact Us</NavItem>
+                        <NavItem eventKey={1} href="/about">About</NavItem>
+                        <NavItem eventKey={2} href="/contacts">Contact Us</NavItem>
                     </Nav>
                     <Nav pullRight>
-                        <NavItem eventKey={1}
-                                 href="/admin">Admin</NavItem>
-                        <NavItem eventKey={2}
-                                 href="/cart">Your Cart
-                            <Badge
-                                className="badge">1</Badge></NavItem>
+                        <NavItem eventKey={1} href="/admin">Admin</NavItem>
+                        <NavItem eventKey={2} href="/cart">Your Cart
+                            {
+                                (this.props.cartItemsNumber > 0) ?
+                                    ( <Badge className="badge">{this.props.cartItemsNumber}</Badge>) :
+                                    ('')
+                            }
+                        </NavItem>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
